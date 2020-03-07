b97e81b10f101793b03d9f8906e6f36d36aff718
ptfrwrd <whtsthmttr@gmail.com>
2020-03-07 12:29:40 +0400

    Setting up React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+        REGION: "us-east-2",
+        BUCKET: "notes-application-ptfrwrd"
+    },
+    apiGateway: {
+        REGION: "us-east-2",
+        URL: "https://nb6nl6qqf2.execute-api.us-east-2.amazonaws.com/prod"
+    },
+    cognito: {
+        REGION: "us-east-2",
+        USER_POOL_ID: "us-east-2_Q9MY7PuPU",
+        APP_CLIENT_ID: "7893dgejnn02j73or50leojvbg",
+        IDENTITY_POOL_ID: "us-east-2:c2c3eccc-ae97-47ad-a5f2-e13ea7fbd3ae"
+    }
+};
